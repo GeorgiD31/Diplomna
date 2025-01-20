3e1ab7eb7f9bd64f4da70c329785465282321d59
GeorgiD31 <georgi.v.dinkov.2020@elsys-bg.org>
2025-01-20 03:31:11 +0200

    added login/register (malko oburkah neshto s breez razbrah kakvo e utre v uchilishte shte go opravq )

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return redirect()->route('register');
 });
 
 
-Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => [
-        ['title' =>'Director',
-        'salary' => '50000'],
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-        ['title' => 'Professor',
-         'salary' => '100000'
-        ]
-
-
-    ]]);});
-
-
-Route::get('/contact', function () {
-    return view('contact');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
